5dd3d0fdd7650232708df754c1c1dbe4bc636acc
Quentin bona <bonaventurequentin@gmail.com>
2022-08-23 16:12:29 +0200

    Ex1 ok

--- EVIOS2/ConnexionViewController.swift
+++ EVIOS2/ConnexionViewController.swift
@@ -9,20 +9,68 @@ import UIKit
 
 class ConnexionViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet  var headerImage: UIImageView!
+    @IBOutlet weak var connectButton: UIButton!
+    
+    var image: String!
     @IBAction func closeButton(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
     }
     
-    var image: String!
+    @IBAction func show(_ sender: Any) {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+         
+          let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        if predicate.evaluate(with: loginTextField.text) && (passwordTextField.text?.count ?? 0) >= 4 {
+              performLogin()
+          } else{
+              showError()
+          }
+       
+    }
+    
+    func performLogin(){
+        if let validLogin = storyboard?.instantiateViewController(withIdentifier: "TabBarControllerViewController"){
+            validLogin.modalPresentationStyle = .fullScreen
+            present(validLogin, animated: true)
+            
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         headerImage.image = UIImage(named: image)
-        // Do any additional setup after loading the view.
+        passwordTextField.delegate = self
+        loginTextField.delegate = self
+        #if DEBUG
+        loginTextField.text = "bonasdfsdf@gmail.com"
+        #endif
     }
     
+ 
+    
+    func showError(){
+        let alert = UIAlertController(title: "Error", message: "Vous n'avez pas bien saisi les champs de ma ndé", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
 
 
+}
+
+extension ConnexionViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        if textField == loginTextField {
+            passwordTextField.becomeFirstResponder()
+        }else if(textField == passwordTextField) {
+            view.endEditing(true)
+        }
+        return true
+    }
 }
---
+++ EVIOS2/PokemonNavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonNavigationController.swift
+//  EVIOS2
+//
+//  Created by Quentin Bona on 23/08/2022.
+//
+
+import UIKit
+
+class PokemonNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
